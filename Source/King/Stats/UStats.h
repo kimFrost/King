5c26aad83d234602dbbc1c4d6289d8172e505d48
kim <kim@impact.dk>
2017-12-04 14:15:44 +0100

    ()

---
+++ Source/King/Stats/UStats.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "UStats.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType)
+class KING_API UStats : public UObject
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
